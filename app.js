8bfb806442c266829257928877a755a865dc9154
Kimaniantony <kimaniantoni@gmail.com>
2022-01-06 17:21:28 +0300

    revamp of styling

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const navToggle = document.querySelector('.nav-toggle');
 const navMenu = document.querySelector('.nav-menu');
 const navClose = document.querySelector('.nav-close');
-
+const telePhone = document.querySelector('.telephone-button');
+const phoneNo = document.querySelector('.telephone-btn p');
 if(navToggle){
     navToggle.addEventListener('click' , ()=>{
         navMenu.classList.add("show-menu");
@@ -12,4 +13,9 @@ if(navClose){
     navClose.addEventListener('click' , ()=>{
         navMenu.classList.remove("show-menu");
     })
+}
+
+telePhone.addEventListener('click') , ()=>{
+    document.body.style.remove('display:none');
+    document.body.style.add('display:block');
 }
