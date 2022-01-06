debcb86e4fe91e37792c24e1057d635eb9ba2865
Kimaniantony <kimaniantoni@gmail.com>
2022-01-06 18:17:04 +0300

    of

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 const navToggle = document.querySelector('.nav-toggle');
 const navMenu = document.querySelector('.nav-menu');
 const navClose = document.querySelector('.nav-close');
-const telePhone = document.querySelector('.telephone-button');
-const phoneNo = document.querySelector('.telephone-btn p');
+
 if(navToggle){
     navToggle.addEventListener('click' , ()=>{
         navMenu.classList.add("show-menu");
@@ -13,9 +12,4 @@ if(navClose){
     navClose.addEventListener('click' , ()=>{
         navMenu.classList.remove("show-menu");
     })
-}
-
-telePhone.addEventListener('click') , ()=>{
-    document.body.style.remove('display:none');
-    document.body.style.add('display:block');
 }
